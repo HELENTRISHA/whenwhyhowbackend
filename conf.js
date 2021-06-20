c7fda143274f4d23aaa637b9b4bbbc23289eb229
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-06-20 16:21:32 +0200

    hiding my password

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "wilhelmine24",
+  password: "***",
   database: "registration",
 });
 module.exports = connection;
