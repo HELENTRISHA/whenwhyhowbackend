bab2f812cab102c3503db77b4576a49415c81057
HELENTRISHA <helen-trisha_student2021@wilder.school>
2021-07-02 11:30:37 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
@@ -13,59 +14,34 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/register", (req, res) => {
-
-  //bcrypt
-  //.hash(req.body.password, 10)
-  //.then((hashedPassword) => {
-  let user = {
-    username: req.body.username,
-    password: req.body.password,
-    description: req.body.description,
-  };
+app.post("/signup", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
   connection.query(
-    `INSERT INTO user SET ? `,
-    [user],
+    `INSERT INTO user (username, password) VALUES (?, ?) `,
+    [req.body.username, hashedPassword],
     (err, results, fields) => {
       if (err) {
-        res.send(err);
+        res.status(500).send(err);
       } else {
-        res.send(user);
+        res.status(201).send(results);
       }
     }
-);
-})
-/*.catch(hasError => console.error(`Error hashing password. Error: ${hasError}`))
+  );
+});
 
-})
+app.post("/login", (res, req) => {
+  console.log("login", req.body);
 
-
-app.post('/login', (req,res) => {
-  console.log('login!', req.body)
-
-  connection.query(`SELECT * from user WHERE username='${req.body.username}'`, (err,results) => {
-    if(err){
-      res.status(500).send('We could not find a user with that username :(')
-    } else {
-      bcrypt
-        .compare(req.body.password, results[0].password)
-        .then((isThereAMatch) => {
-          if(isThereAMatch){
-            res.json({
-              username: results[0].username,
-              description: results[0].description,
-              message:'You have successfully logged in!',
-              token: '12863vf178dfb1092d8h1º9d'
-            })
-          } else {
-            res.send('Wrong password, get out!')
-          }
-        })
-    }
-  })
-})*/
-
+  connection.query(`SELECT* from user WHERE  ${req.body.username}`),
+    (err, results) => {
+      if (err) {
+        res.status(500).send("we could not find your username");
+      } else {
+        console.log(results);
+      }
+    };
+});
 
 app.listen(port, (err) => {
   if (err) throw new Error("ups something is not working");
